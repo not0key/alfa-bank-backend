40e8c3e0b6bdc132d70bf8b881341238da2ddfd3
Даниил <obuhov@it-ip.ru>
2024-12-10 22:39:44 +0500

    develop: add chatgpt functional

--- core/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-TEMP_DIR = BASE_DIR / "temp"
-
-# Проверяем, существует ли папка temp
-TEMP_DIR.mkdir(exist_ok=True)
