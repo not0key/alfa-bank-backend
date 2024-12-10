83cdd3e987f233a71cb86c639ec3061790faf459
Даниил <obuhov@it-ip.ru>
2024-12-11 01:24:41 +0500

    master: edit api requests

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ from api import generative_model, file_parser
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
 app.include_router(file_parser.router, prefix="/file_parsing", tags=["File Parsing"])
 
 
@@ -24,3 +13,4 @@ app.include_router(generative_model.router, prefix="/ChatGpt", tags=["ChatGpt"])
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+app.include_router(generative_model.router, prefix="/GenerativeModel", tags=["GenerativeModel"])
